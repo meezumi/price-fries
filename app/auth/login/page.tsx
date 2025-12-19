0d8d9643979fb20c4dbcbc5171069721712ac818
meezumi <aaryank3000@gmail.com>
2025-12-19 17:44:42 +0530

    feat: Add dynamic export to login and verify pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+export const dynamic = 'force-dynamic';
+
 import React, { FormEvent, useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
